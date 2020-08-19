d15ba2f9bca6893647fbe09356afd5f0e4914bf8
Duncan Gray <duncan.gray@ga.gov.au>
2020-08-19 17:54:28 +1000

    setting up for the dev platform

--- Makefile
+++ Makefile
@@ -4,13 +4,18 @@ run-air:
 	-v /home/osboxes/sandbox/dea-airflow/dags:/usr/local/airflow/dags \
 	-v /home/osboxes/sandbox/dea-airflow/plugins:/usr/local/airflow/plugins \
 	--env FERNET_KEY \
-	--name dea-airflow_webserver_1 \
+	--name dea-airflow_puckel \
 	puckel/docker-airflow
 start-air:
-	docker start dea-airflow_webserver_1
+	docker start dea-airflow_puckel
 stop-air:
-	docker stop dea-airflow_webserver_1
+	docker stop dea-airflow_puckel
 rm-air:
-	docker rm dea-airflow_webserver_1
+	docker rm dea-airflow_puckel
 bash-air:
-	docker exec   --env FERNET_KEY  -ti dea-airflow_webserver_1 bash
+	docker exec --env FERNET_KEY -ti dea-airflow_puckel bash
+dcup:
+	docker-compose up -d
+dcdown:
+	docker-compose down
+
--- dags/nci_ard_lpgs.py
+++ dags/nci_ard_lpgs.py
@@ -15,11 +15,11 @@ default_args = {
     'start_date': datetime(2020, 2, 17),
     'retries': 0,
     'retry_delay': timedelta(minutes=1),
+    #'ssh_conn_id': 'lpgs_gadi',
     'ssh_conn_id': 'dsg547',
     'params': {
-        'project': 'u46',
+        'project': 'v10',
         'queue': 'normal',
-        'module': 'dea/unstable',
         'module_ass': 'ard-scene-select-py3-dea/20200814',
         'index_arg': '--index-datacube-env /g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/index-datacube.env',
         'wagl_env': '/g/data/v10/projects/c3_ard/dea-ard-scene-select/scripts/prod/ard_env/prod-wagl.env',
@@ -29,12 +29,12 @@ default_args = {
 # tags is in airflow >1.10.8
 # My local env is airflow 1.10.10...
 dag = DAG(
-    'nci_ard',
+    'nci_ard_lpgs',
     default_args=default_args,
     catchup=False,
     schedule_interval=None,
     default_view='graph',
-    tags=['nci', 'landsat_32'],
+    tags=['nci', 'landsat_c3'],
 )
 
 with dag:
@@ -42,13 +42,11 @@ with dag:
     completed = DummyOperator(task_id='completed')
 
     COMMON = """
-        #  ts_nodash timestamp no dash.
-        {% set log_dir = '/home/547/dsg547/dump/airflow/' + ts_nodash %}
-        {% set work_dir = '/home/547/dsg547/dump/airflow' %}
-    
-        #module use /g/data/v10/public/modules/modulefiles;
-        #module load {{ params.module }};
-
+        #  ts_nodash timestamp no dashes.
+        #{% set log_dir = '/g/data/v10/Landsat-Collection-3-ops/scene_select_test/' + ts_nodash + '/logdir' %}
+        #{% set work_dir = '/g/data/v10/Landsat-Collection-3-ops/scene_select_test/' + ts_nodash + '/workdir' %}
+        {% set log_dir = '/home/547/dsg547/dump/airflow/' + ts_nodash + '/logdir' %}
+        {% set work_dir = '/home/547/dsg547/dump/airflow/' + ts_nodash + '/workdir' %}
         """
 
     product = 'used_by_params'
@@ -56,6 +54,7 @@ with dag:
         command=COMMON + """
         # make the dir here so the output from the submit task is minimal
         mkdir -p {{ log_dir }}
+        mkdir -p {{ work_dir }}
         """,
         params={},
         task_id=f'set_up',
@@ -68,6 +67,7 @@ with dag:
         task_id=submit_task_id,
         command=COMMON + """
         mkdir -p {{ log_dir }} # this is silent, this is good
+        mkdir -p {{ work_dir }} # this is silent, this is good
         qsub -N ard_scene_select \
               -q  {{ params.queue }}  \
               -W umask=33 \
@@ -78,8 +78,8 @@ with dag:
                   "module use /g/data/v10/public/modules/modulefiles/; \
                   module use /g/data/v10/private/modules/modulefiles/; \
                   module load {{ params.module_ass }}; \
-                  ard-scene-select --workdir {{ log_dir }} \
-                  --pkgdir {{ log_dir }} --logdir {{ log_dir }} \
+                  ard-scene-select --workdir {{ work_dir }} \
+                  --pkgdir {{ work_dir }} --logdir {{ log_dir }} \
                   --env {{ params.wagl_env }}  \
                    --project {{ params.project }} \
                    --walltime 05:00:00"
